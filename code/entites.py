1c4df807a1d5981469d29edbb157a930e7ea3725
Harsh9933 <harshmishra9933@gmail.com>
2024-05-09 18:16:24 +0530

    Character

--- code/entites.py
+++ code/entites.py
@@ -1,16 +1,44 @@
+import os.path
+
 import pygame
 
 from settings import *
+from os.path import join
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, gropus):
-        super().__init__(gropus)
-        self.image = pygame.Surface((100, 100))
-        self.image.fill('green')
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, pos, frames, groups):
+        super().__init__(groups)
+        self.direction = vector()
+        self.facing_direction = 'down'
+        self.speed = 250
+        self.frame_index, self.frames = 0, frames
+        self.image = self.frames[self.get_state()][self.frame_index]
         self.rect = self.image.get_frect(center=pos)
 
-        self.direction = vector()
+    def animate(self, dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[self.get_state()][int(self.frame_index % len(self.frames[self.get_state()]))]
+
+    def get_state(self):
+        moving = bool(self.direction)
+        if moving:
+            self.next = ''
+            if self.direction.x != 0:
+                self.facing_direction = 'right' if self.direction.x > 0 else 'left'
+            if self.direction.y != 0:
+                self.facing_direction = 'down' if self.direction.y > 0 else 'up'
+        else:
+            self.next = '_idle'
+
+        return f'{self.facing_direction}{self.next}'
+
+
+class Player(Entity):
+    def __init__(self, pos, frames, groups):
+        super().__init__(pos, frames, groups)
+
+
     def input(self):
         keys = pygame.key.get_pressed()
         input_vector = vector(0, 0)
@@ -25,8 +53,16 @@ class Player(pygame.sprite.Sprite):
         self.direction = input_vector
 
     def move(self, dt):
-        self.rect.center += self.direction * 250 * dt
+        self.rect.center += (self.direction * self.speed * dt)
 
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
+
+
+class Character(Entity):
+    def __init__(self, pos, frames, groups):
+        super().__init__(pos, frames, groups)
+
+
--- code/main.py
+++ code/main.py
@@ -3,9 +3,11 @@ import pygame
 from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
-from sprites import Sprite
-from entites import Player
+from sprites import Sprite,AnimatedSprite
+from entites import Player,  Character
 from groups import AllSprites
+from support import *
+
 
 class Game:
     def __init__(self):
@@ -21,23 +23,60 @@ class Game:
         self.setup(self.tmx_maps['world'], 'house')
 
     def import_assets(self):
-        self.tmx_maps = {'world': load_pygame(join('..', 'data', 'maps', 'world.tmx'))}
+        self.tmx_maps = {
+            'world': load_pygame(join('..', 'data', 'maps', 'world.tmx')),
+            'hospital': load_pygame(join('..', 'data', 'maps', 'hospital.tmx'))
+        }
+        self.overworld_frames = {
+            'water': import_folder('..', 'graphics', 'tilesets', 'water'),
+            'coast': coast_importer(24, 12, '..', 'graphics', 'tilesets', 'coast'),
+            'characters': all_character_import('..', 'graphics', 'characters')
+        }
+
+
 
     def setup(self, tmx_map, player_start_pos):
         for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
 
-        # for obj in tmx_map.get_layer_by_name('Objects').tiles():
-        #     Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+        for x, y, surf in tmx_map.get_layer_by_name('Terrain Top').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+
+        for obj in tmx_map.get_layer_by_name('Objects'):
+            Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+
+
+        for obj in tmx_map.get_layer_by_name('Water'):
+            for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE):
+                for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
+                    AnimatedSprite((x, y), self.overworld_frames['water'], self.all_sprites)
+
+
+        for obj in tmx_map.get_layer_by_name('Coast'):
+            terrain = obj.properties['terrain']
+            side = obj.properties['side']
+            AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.all_sprites)
 
         for obj in tmx_map.get_layer_by_name('Entities'):
-            if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
-                self.player = Player((obj.x, obj.y),self.all_sprites)
+            graphic = obj.properties.get('graphic', None)
+            if obj.name == 'Player':
+                if obj.properties['pos'] == player_start_pos:
+                    self.player = Player(
+                        pos=(obj.x, obj.y),
+                        frames=self.overworld_frames['characters']['player'],
+                        groups=self.all_sprites)
+            else:
+                 self.character = Character(
+                    pos=(obj.x, obj.y),
+                    frames=self.overworld_frames['characters'][graphic],
+                    groups=self.all_sprites
+                )
+        
 
 
     def run(self):
         while True:
-            dt = self.clock.tick() /1000
+            dt = self.clock.tick() / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -45,7 +84,6 @@ class Game:
             self.all_sprites.update(dt)
             self.display_surface.fill('black')
             self.all_sprites.draw(self.player.rect.center)
-            print(self.clock.get_fps())
             pygame.display.update()
 
 
